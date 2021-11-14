e9de38c14f8e236e2500a6c789af10b4d62c65fa
tiantiaf <tiantiaf@usc.edu>
2021-11-14 01:02:24 +0000

    clean up opensmile feature extraction

---
+++ feature_extraction/opensmile_feature_extraction.sh
@@ -0,0 +1,9 @@
+# This is the script for opensmile feature extraction
+
+# audio feature part
+# three datasets are iemocap, crema-d, and msp-improv dataset
+# features include emobase, ComParE, and gemap opensmile features
+
+taskset 100 python3 opensmile_feature_extraction.py --dataset msp-improv --feature_type ComParE
+taskset 100 python3 opensmile_feature_extraction.py --dataset iemocap --feature_type ComParE
+taskset 100 python3 opensmile_feature_extraction.py --dataset crema-d --feature_type ComParE
